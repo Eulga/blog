152f24ee8c72ae9bd9bb952f9b195834a8832d1f
Dsys1129 <dsys1129@gmail.com>
2023-06-27 15:48:34 +0900

    feat : add User Entity

---
+++ src/main/java/com/example/bloghw2/user/entity/User.java
@@ -0,0 +1,35 @@
+package com.example.bloghw2.user.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "users")
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String userPassword;
+
+    @Builder
+    private User(String username, String userPassword) {
+        this.username = username;
+        this.userPassword = userPassword;
+    }
+}
