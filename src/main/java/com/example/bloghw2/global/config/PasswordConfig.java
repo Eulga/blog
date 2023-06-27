8c99ea84a252f8143990e4001991db6143bd3589
Dsys1129 <dsys1129@gmail.com>
2023-06-27 18:29:08 +0900

    fix : PasswordConfig package 변경

--- src/main/java/com/example/bloghw2/global/config/PasswordConfig.java
+++ src/main/java/com/example/bloghw2/global/config/PasswordConfig.java
@@ -1,4 +1,4 @@
-package com.example.bloghw2.user.config;
+package com.example.bloghw2.global.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
