89f05a91cdb8dee9cae66ae32fb561e76bde43c1
Dsys1129 <dsys1129@gmail.com>
2023-06-28 11:35:14 +0900

    feat : add WebConfig

---
+++ src/main/java/com/example/bloghw2/global/config/WebConfig.java
@@ -0,0 +1,26 @@
+package com.example.bloghw2.global.config;
+
+import java.util.List;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import com.example.bloghw2.jwtutil.LoginUserArgumentResolver;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(new LoginUserArgumentResolver());
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+}
