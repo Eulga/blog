611c167b852981817c1329488664963c6b2f56cc
KH <106093449+KH-CHOO@users.noreply.github.com>
2023-07-04 17:06:36 +0900

    feat : 회원가입시 role 부여

--- src/main/java/com/example/bloghw2/user/controller/UserController.java
+++ src/main/java/com/example/bloghw2/user/controller/UserController.java
@@ -1,20 +1,20 @@
 package com.example.bloghw2.user.controller;
 
+import com.example.bloghw2.user.dto.LoginRequestDTO;
+import com.example.bloghw2.user.dto.LoginResponseDTO;
+import com.example.bloghw2.user.dto.SignupRequestDTO;
+import com.example.bloghw2.user.dto.SignupResponseDTO;
+import com.example.bloghw2.user.service.UserService;
+import jakarta.servlet.http.HttpServletResponse;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.example.bloghw2.jwtutil.JwtProvider;
-import com.example.bloghw2.user.dto.LoginResponseDTO;
-import com.example.bloghw2.user.dto.UserRequestDTO;
-import com.example.bloghw2.user.dto.UserResponseDTO;
-import com.example.bloghw2.user.service.UserService;
-
-import jakarta.validation.Valid;
-import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 @RestController
@@ -24,15 +24,14 @@ public class UserController {
     private final UserService userService;
 
     @PostMapping("/signup")
-    public ResponseEntity<UserResponseDTO> signup(@Valid @RequestBody UserRequestDTO userRequestDTO){
-        UserResponseDTO response = userService.signup(userRequestDTO);
+    public ResponseEntity<SignupResponseDTO> signup(@Valid @RequestBody SignupRequestDTO signupRequestDTO){
+        SignupResponseDTO response = userService.signup(signupRequestDTO);
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
 
     @PostMapping("/login")
-    public ResponseEntity<UserResponseDTO> login(@Valid @RequestBody UserRequestDTO userRequestDTO){
-        LoginResponseDTO loginResponse = userService.login(userRequestDTO);
-        UserResponseDTO responseBody = new UserResponseDTO(loginResponse.getSuccess(), loginResponse.getStatus());
-        return ResponseEntity.status(HttpStatus.OK).header(JwtProvider.AUTHORIZATION_HEADER, loginResponse.getAccessToken()).body(responseBody);
+    public ResponseEntity<LoginResponseDTO> login(@Valid @RequestBody LoginRequestDTO loginRequestDTO, HttpServletResponse res){
+        LoginResponseDTO response = userService.login(loginRequestDTO, res);
+        return ResponseEntity.status(HttpStatus.OK).body(response);
     }
 }
