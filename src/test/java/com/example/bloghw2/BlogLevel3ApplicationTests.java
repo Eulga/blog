b8739d1f04909595c847e0512255008fb75a29f0
Eulga <kimyeoun10@gmail.com>
2023-07-04 20:57:29 +0900

    메인 클래스 이름 변경 BlogHw2Application.java -> BlogLevel3Application

--- src/test/java/com/example/bloghw2/BlogLevel3ApplicationTests.java
+++ src/test/java/com/example/bloghw2/BlogLevel3ApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class BlogHw2ApplicationTests {
+class BlogLevel3ApplicationTests {
 
     @Test
     void contextLoads() {
