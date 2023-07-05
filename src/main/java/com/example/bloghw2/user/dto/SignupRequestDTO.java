3e7df8d887472a8bae866ec43e61e0f850dc28f7
KH <106093449+KH-CHOO@users.noreply.github.com>
2023-07-05 15:46:52 +0900

    refactor : @Pattern 수정

--- src/main/java/com/example/bloghw2/user/dto/SignupRequestDTO.java
+++ src/main/java/com/example/bloghw2/user/dto/SignupRequestDTO.java
@@ -6,12 +6,14 @@ import lombok.Getter;
 
 @Getter
 public class SignupRequestDTO {
-
-    @Pattern(regexp = "^(?=.*[0-9])(?=.*[a-zA-Z]).{4,10}$", message = ("영문과 숫자 조합으로 작성하셔야 합니다."))
+    @Pattern(regexp = "^(?=.*[0-9])(?=.*[a-z])"
+            + "[0-9a-z]{4,10}$"
+            , message = "유저명 규칙에 어긋남")
     private final String username;
 
-    //특수문자 정규표현식 https://mkyong.com/regular-expressions/how-to-validate-password-with-regular-expression/
-    @Pattern(regexp = "^(?=.*[0-9])(?=.*[a-zA-Z])(?=.*[!\"#$%&'()*+,\\-./:;<=>?@\\[₩\\]^_`{|}~]).{8,15}$")
+    @Pattern(regexp = "^(?=.*[0-9])(?=.*[a-zA-Z])(?=.*[!\"#$%&'()*+,\\-./:;<=>?@\\[₩\\]^_`{|}~])"
+            + "[0-9a-zA-Z!\"#$%&'()*+,\\-./:;<=>?@\\[₩\\]^_`{|}~]{8,15}$"
+            , message = "비밀번호 규칙에 어긋남")
     private final String password;
 
     private boolean admin = false;
