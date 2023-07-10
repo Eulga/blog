ee4b373cc931ea50a27ebc6198c884d6eb1733e9
KH <106093449+KH-CHOO@users.noreply.github.com>
2023-07-11 03:22:37 +0900

    refactor : 게시글, 댓글
    시큐리티 적용

--- src/main/java/com/example/bloghw2/domain/post/controller/PostController.java
+++ src/main/java/com/example/bloghw2/domain/post/controller/PostController.java
@@ -5,8 +5,10 @@ import java.util.Map;
 
 import com.example.bloghw2.domain.comment.service.CommentService;
 import com.example.bloghw2.domain.post.service.PostService;
+import com.example.bloghw2.global.security.UserDetailsImpl;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -33,8 +35,8 @@ public class PostController {
     // 게시글 생성
     @PostMapping("/posts")
     public ResponseEntity<PostResponseDTO> createPost(@Valid @RequestBody PostRequestDTO postRequestDTO,
-                                                      String username) {
-        PostResponseDTO response = postService.createPost(postRequestDTO, username);
+                                                      @AuthenticationPrincipal UserDetailsImpl userDetails) {
+        PostResponseDTO response = postService.createPost(postRequestDTO, userDetails.getUsername());
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
 
@@ -56,8 +58,8 @@ public class PostController {
     @PutMapping("/posts/{postId}")
     public ResponseEntity<PostResponseDTO> modifyPost(@PathVariable("postId") Long postId
                                                     ,@Valid @RequestBody PostRequestDTO postRequestDTO
-                                                    , String username) {
-        PostResponseDTO response = postService.modifyPost(postId, postRequestDTO, username);
+                                                    ,  @AuthenticationPrincipal UserDetailsImpl userDetails) {
+        PostResponseDTO response = postService.modifyPost(postId, postRequestDTO, userDetails.getUsername());
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
 
@@ -65,8 +67,8 @@ public class PostController {
     // 게시글 삭제
     @DeleteMapping("/posts/{postId}")
     public ResponseEntity<Map<String,String>> deletePost(@PathVariable("postId") Long postId
-                                                        , String username){
-        Map<String, String> response = postService.deletePost(postId, username);
+                                                        ,  @AuthenticationPrincipal UserDetailsImpl userDetails){
+        Map<String, String> response = postService.deletePost(postId, userDetails.getUsername());
         return ResponseEntity.status(HttpStatus.OK).body(response);
     }
 }
