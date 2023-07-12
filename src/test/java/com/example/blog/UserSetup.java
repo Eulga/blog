b297a0c46007b8acc392d36273dbe1f74be1ee50
Eulga <kimyeoun10@gmail.com>
2023-07-12 20:32:41 +0900

    rename: 프로젝트 이름 수정
    bloghw2-> blog
    blog-level4-> blog
    
    branch 로 level3, level4 보관

--- src/test/java/com/example/blog/UserSetup.java
+++ src/test/java/com/example/blog/UserSetup.java
@@ -13,7 +13,7 @@ public class UserSetup {
     private UserRepository userRepository;
 
     public Long saveUser(User user){
-        return userRepository.save(user).getUserId();
+        return userRepository.save(user).getId();
     }
 
     public void clearUsers(){
